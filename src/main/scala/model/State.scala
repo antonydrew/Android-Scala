c5f856d8536fe6aab7c9bdf70c287ac61bacd6e2
adrew <adrew@localhost>
2015-05-07 16:08:12 -0700

    Thinking of GRIDVIEW to draw board

--- src/main/scala/model/State.scala
+++ src/main/scala/model/State.scala
@@ -14,7 +14,7 @@ object State {
 -Use ASYNC in background that sends onTick() event to ALL monsters at once so each monster is a little state
 -2 main states: MOVE [can I?] and VULNERABLE [to TOUCH event]...if can move then invoke/create onMove() method [which uses RAND square jump]
 -Use BINARY SEMAPHORE to make sure only 1 monster allowed [or filling] each square
--Once abstract rep is done for 2D grid, then study onDraw API to figure out how to render board in MAIN.XML...
+-Once abstract rep is done for 2D grid, then study onDraw API to figure out how to render board in MAIN.XML...USE GRIDVIEW to show squares and alternate colors!
 
  */
 
